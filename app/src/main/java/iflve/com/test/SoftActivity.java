4961dcfbdaa8654d9cda3e677e11abe928a0ece7
zhonggedan <184341327@qq.com>
2016-03-31 10:29:00 +0800

    xxjue1

---
+++ app/src/main/java/iflve/com/test/SoftActivity.java
@@ -0,0 +1,22 @@
+package iflve.com.test;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class SoftActivity extends AppCompatActivity {
+    private Button buttonCamera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft);
+
+
+    }
+   private void create(){
+
+   }
+
+}
